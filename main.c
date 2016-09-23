108f3e48daa16dd2d11b9cfd23796beb2c20e34a
ldusk <chris.shields1@gmail.com>
2016-09-23 09:14:02 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,33 +1,7 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
 /*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
+todo: ensure that the analog return value from getWind is a float between 0 and 5. 
  */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
+ 
 #include <asf.h>
 #include <gpio.h>
 #include <inttypes.h>
